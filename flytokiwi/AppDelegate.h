28462cdc8a78a9ec9f2dbb507349b40e40e48103
WyattZhang <zwuquan@gmail.com>
2014-10-04 12:08:55 +0800

    Initial Commit

---
+++ flytokiwi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flytokiwi
+//
+//  Created by Q on 14-10-4.
+//  Copyright (c) 2014年 zwq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ flytokiwi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  flytokiwi
+//
+//  Created by Q on 14-10-4.
+//  Copyright (c) 2014年 zwq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
