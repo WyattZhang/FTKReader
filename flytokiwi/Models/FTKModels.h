777b51c948c36547eccf9e230362bad2e350dfbd
WyattZhang <zwuquan@gmail.com>
2014-10-06 10:56:26 +0800

    Add  FTKModels

---
+++ flytokiwi/Models/FTKModels.h
@@ -0,0 +1,13 @@
+//
+//  FTKModels.h
+//  flytokiwi
+//
+//  Created by Q on 14-10-6.
+//  Copyright (c) 2014年 zwq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FTKModels : NSObject
+
+@end
---
+++ flytokiwi/Models/FTKModels.m
@@ -0,0 +1,13 @@
+//
+//  FTKModels.m
+//  flytokiwi
+//
+//  Created by Q on 14-10-6.
+//  Copyright (c) 2014年 zwq. All rights reserved.
+//
+
+#import "FTKModels.h"
+
+@implementation FTKModels
+
+@end
